207f71da640b4b275872e289f86da62231c47803
Brenobn <brenobragabn@gmail.com>
2025-07-13 15:18:42 -0300

    Importing env and sql, and using seed to build the table schema

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,7 +1,24 @@
 import { reset, seed } from 'drizzle-seed'
-import { db } from './connection.ts'
+import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
+import { env } from '../env.ts'
 
-await reset(db, { schema })
+await reset(db, schema)
 
-await seed(db, { schema })
+await seed(db, schema).refine(f => {
+  return {
+    rooms: {
+      count: 20, 
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  }
+})
+
+await sql.end()
+
+if (env.NODE_ENV === 'development') {
+  console.log('Database seeded')
+}
